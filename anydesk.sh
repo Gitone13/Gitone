4266e5dcc55a0ed9c8593cdce7367e1055f5c2c9
Gitone13 <136300713+Gitone13@users.noreply.github.com>
2023-06-14 15:22:55 +0530

    Update anydesk.sh

--- anydesk.sh
+++ anydesk.sh
@@ -6,7 +6,7 @@ sudo dpkg -i suraj-anydesk.deb
 sudo rm suraj-anydesk.deb 
 sudo apt --fix-broken install -y 
 echo " installation of anydesk complete "
-
+gsettings set org.gnome.desktop.background picture-uri https://raw.githubusercontent.com/ranjanjyoti152/opencvproxpc/main/Wallpaper-01.jpg
 echo " setting Access Password = prox@123 "
 echo prox@123 | sudo anydesk --set-password 
 echo " viewing Anydesk id "
